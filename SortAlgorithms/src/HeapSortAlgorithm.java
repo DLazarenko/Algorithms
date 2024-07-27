59c3bdfc471a603ee986efdaf6792572e207d163
Daria <daria.lazarenko20@gmail.com>
2024-07-27 10:34:31 -0700

    Heap sort for String added

--- SortAlgorithms/src/HeapSortAlgorithm.java
+++ SortAlgorithms/src/HeapSortAlgorithm.java
@@ -1,13 +1,27 @@
 public class HeapSortAlgorithm {
 
     public static void main(String[] args) {
-        sort(new int[]{1,12,9,5,6,10});
+        sort(new int[]{22, 5, 1, 17});
     }
 
     public static void sort(int[] arr){
         int N = arr.length;
+        // 1. Build the heap from the array
         for(int i = N/2-1; i >= 0; i--){
             heapify(arr, N, i);
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i] + " first sort");
+        }
+        for(int i = N-1; i > 0; i--){
+            // 2. Put the largest element at the end of the heap
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            // 3. Cut off last element of the heap and reduce its size by 1
+            // 4. Heapify the root element again
+            heapify(arr, i, 0);
+            // 5. Repeat the process until all the elements are sorted
         }
         for(int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
