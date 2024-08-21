ed5e0b0e057a7b39b5b1758a000f3d25e19e1eda
DLazarenko <daria.lazarenko20@gmail.com>
2024-08-21 08:36:53 -0700

    Collinear project added

--- Theory/src/Student.java
+++ Theory/src/Student.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Comparator;
 
 public class Student {
+
+    public static final Comparator<Student> BY_NAME = new ByName();
+    public static final Comparator<Student> BY_SECTION = new BySection();
+
+    public Student(String name, int section) {
+        this.name = name;
+        this.section = section;
+    }
+    private final String name;
+    private final int section;
+
+    private static class ByName implements Comparator<Student>{
+        public int compare(Student v, Student w){
+            return v.name.compareTo(w.name);
+        }
+    }
+    private static class BySection implements Comparator<Student>{
+        public int compare(Student v, Student w){
+            return v.section - w.section;
+        }
+    }
 }
